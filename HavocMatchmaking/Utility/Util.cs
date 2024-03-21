a74595378040f6e4e14dbad5de2bc944f21cb0dc
AlexRak2 <alejandrorak2@gmail.com>
2024-03-20 23:38:42 -0400

    added silver to bronze queue

--- HavocMatchmaking/Utility/Util.cs
+++ HavocMatchmaking/Utility/Util.cs
@@ -7,7 +7,7 @@ public static class Util
         return rank switch
         {
             >= 0 and < 150 => 0,
-            >= 150 and < 300 => 1,
+            >= 150 and < 300 => 0, //should be one doing 0 for playtest
             >= 300 and < 450 => 2,
             >= 450 and < 600 => 3,
             >= 600 and < 750 => 4,
