a6af15fbaed5116eee5139779c7d2b3f22c46a73
AlexRak2 <alejandrorak2@gmail.com>
2024-03-27 17:54:48 -0400

    all ranks can queue together

--- HavocMatchmaking/Utility/Util.cs
+++ HavocMatchmaking/Utility/Util.cs
@@ -8,10 +8,10 @@ public static class Util
         {
             >= 0 and < 150 => 0,
             >= 150 and < 300 => 0, //should be one doing 0 for playtest
-            >= 300 and < 450 => 2,
-            >= 450 and < 600 => 3,
-            >= 600 and < 750 => 4,
-            >= 750 and < 900 => 5,
+            >= 300 and < 450 => 0,
+            >= 450 and < 600 => 0,
+            >= 600 and < 750 => 0,
+            >= 750 and < 900 => 0,
             _ => 0
         };
     }
